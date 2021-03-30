4dab12683872309e5fc5033a81e81c29af4d833e
Mantas <mantasu11@gmail.com>
2021-03-30 15:47:05 +0300

    array

---
+++ js/05-array.js
@@ -0,0 +1,18 @@
+/*
+
+
+*/
+
+
+const marks = [10, 2, 8, 4, 6, 7];
+console.log(marks);
+
+const firstMark = marks[0];
+console.log('Pirmasis pazymys:', firstMark);
+
+const secondMark = marks[1];
+console.log('Antrasis pazymys:', secondMark);
+
+const lastMarkIndex = marks.length - 1;
+const lastMark = marks[lastMarkIndex];
+console.log('Paskutinis pazymys:', lastMark);
